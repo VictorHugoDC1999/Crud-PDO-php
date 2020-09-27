c2647d0d5b682f2d1f87184bebc72afd1368de72
Victor <victorhugodelciampo@gmail.com>
2020-09-27 12:12:42 -0300

    Third Commit

--- App/Model/ProductDao.php
+++ App/Model/ProductDao.php
@@ -8,16 +8,47 @@ class ProductDao {
 
         $sql = 'INSERT INTO products (name, description) VALUES (?,?)';
 
-        $insert = Connection::getConn() -> prepare($sql);
-        $insert -> bindValue(1, $product -> getName());
-        $insert -> bindValue(2, $product -> getDescription());
-        $insert -> execute();
+        $register = Connection::getConn() -> prepare($sql);
+        $register -> bindValue(1, $product -> getName());//este valor é equivalente a primeira interrogação do VALUES da linha 9, assim por diante
+        $register -> bindValue(2, $product -> getDescription());
+        $register -> execute();
 
     }
 
-    public function read() {}
+    public function read() {
 
-    public function update(Product $product) {}
+        $sql = 'SELECT * FROM products';
 
-    public function delete($id) {}
+        $register = Connection::getConn() -> prepare($sql);
+        $register -> execute();
+
+        if ($register -> rowCount() > 0){//se houver pelo menos um registro no banco de dados
+            $result = $register -> fetchAll(\PDO::FETCH_ASSOC);
+            return $result;
+        } else {
+            return [];
+        }
+
+    }
+
+    public function update(Product $product) {
+
+        $sql = 'UPDATE products SET name = ?, description = ? WHERE id = ?';
+
+        $register = Connection::getConn() -> prepare($sql);
+        $register -> bindValue(1, $product -> getName());//associando nossos bindValues as nossas ? da linha 36
+        $register -> bindValue(2, $product -> getDescription());
+        $register -> bindValue(3, $product -> getId());
+
+        $register -> execute();
+    }
+
+    public function delete($id) {
+
+        $sql = 'DELETE FROM products WHERE id = ?';
+
+        $register = Connection::getConn() -> prepare($sql);
+        $register -> bindValue(1, $id);
+        $register -> execute();
+    }
 }
--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 
 require_once 'vendor/autoload.php';
 
-$product = new \App\Model\Product();
-$product -> setName('Microfone AT2020');
-$product -> setDescription('Microfone Condensador');
-
 $productDao = new \App\Model\ProductDao();
-$productDao -> create($product);
+$productDao -> delete(2);
+$productDao -> read();
+
+foreach ($productDao -> read() as $product) {
+    echo $product['name']. "<br>" .$product['description']."<hr>";
+}
